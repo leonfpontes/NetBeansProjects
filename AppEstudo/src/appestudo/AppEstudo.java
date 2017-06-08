0615281743653aca1cc18211cc677b8287e7a58e
Leonardo Fonseca Pontes <leonfpontes@gmail.com>
2017-06-07 21:16:01 -0300

    07-06-2017

--- AppEstudo/src/appestudo/AppEstudo.java
+++ AppEstudo/src/appestudo/AppEstudo.java
@@ -2,29 +2,35 @@ package appestudo;
 
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Rectangle;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class AppEstudo extends JFrame{
 
     private JPanel p1;
-    
+
     public AppEstudo(){
-        
+
         setSize(500,400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new FlowLayout(FlowLayout.LEFT));
-        
+        setBackground(Color.red);
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+
         p1 = new JPanel();
-        p1.setSize(200, 200);
+        p1.setBounds(new Rectangle(100, 100));
         p1.setBackground(Color.blue);
         
+        JButton button = new JButton("Alguma coisa");
+        p1.add(button);
+
         add(p1);
     }
-    
+
     public static void main(String[] args) {
         AppEstudo app = new AppEstudo();
         app.setVisible(true);
     }
-    
+
 }
